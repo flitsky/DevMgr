ea29941458fffa1e0ee717237155c5fdbf59e724
Ryan Kim <flitsky@naver.com>
2017-09-09 18:33:02 +0900

    remove unnecessary json file

--- src/Aries/DeviceManager/DevMgr.java
+++ src/Aries/DeviceManager/DevMgr.java
@@ -25,89 +25,90 @@ public class DevMgr {
 		
 		//-- 1.[Send REQ] Discovery Device
 		//{"type":"req","dir":"d2c","work_code":"dis_dev","body":{}}
-		sendReqDiscoveryDevice();
-		//-- 2.[Recv Resp] Discovery Device Result
-		//{"type":"res","dir":"c2d","work_code":"dis_dev","body":{"status":200,"devices":[{"dev_id":"c31e8fa3-b524-0e6b-2489-77760c3ca37b","dev_name":"THU Light","spec_ver":"ocf.1.1.0","dev_type":["oic.wk.d", "oic.d.light"],"host_addr":"coap:\/\/[fe80::c4a8:5af:7d0e:f40e%25wlan0]:49244"}]}}
-		recvRespDiscoveryDevice();
-		//-- 3.[Send REQ] Discovery Resource
-		//{"type":"req","dir":"d2c","work_code":"dis_res","body":{"dev_id":"6341cb6f-2179-55a3-3732-c3ffbad1be68"}}
-		sendReqDiscoveryResource();
-		//-- 4.[Recv Resp] Discovery Resource Result
-		//{"type":"res","dir":"c2d","work_code":"dis_res","body":{"status":200,"resources":[{"res_id":"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0","res_type":["oic.r.switch.binary"],"res_uri":"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0","res_isobserve":true,"res_dev_id":"6341cb6f-2179-55a3-3732-c3ffbad1be68"}]}}
-		recvRespDiscoveryResource();
-	}
-	
-	static void sendReqDiscoveryDevice() {
 		Schema0Cmd sendReqSchema0Cmd = new Schema0Cmd();
 		sendReqSchema0Cmd.type = "req";
 		sendReqSchema0Cmd.direction = "d2c";
 		sendReqSchema0Cmd.work_code = "dis_dev";
-		// Export jsonObject
+		sendCommand(sendReqSchema0Cmd);
+
+		//-- 2.[Recv Resp] Discovery Device Result
+		//{"type":"res","dir":"c2d","work_code":"dis_dev","body":{"status":200,"devices":[{"dev_id":"c31e8fa3-b524-0e6b-2489-77760c3ca37b","dev_name":"THU Light","spec_ver":"ocf.1.1.0","dev_type":["oic.wk.d", "oic.d.light"],"host_addr":"coap:\/\/[fe80::c4a8:5af:7d0e:f40e%25wlan0]:49244"}]}}
+		JSONObject receivedJsonObj = recvCommand(1);
+		Schema0Cmd recvSchema0Cmd = JsonObj2EntityX(receivedJsonObj);
+		JSONObject jsonExportBody;
 		try {
-			JSONObject JsonObj4Req = sendReqSchema0Cmd.exportToJson();
-			// check exported data
-			logger.debug(" 1 send request = " + JsonObj4Req.toString());
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	}
-	
-	static void recvRespDiscoveryDevice() {
-		String buffer = "{\"type\":\"res\",\"dir\":\"c2d\",\"work_code\":\"dis_dev\",\"body\":{\"status\":200,\"devices\":[{\"dev_id\":\"c31e8fa3-b524-0e6b-2489-77760c3ca37b\",\"dev_name\":\"THU Light\",\"spec_ver\":\"ocf.1.1.0\",\"dev_type\":[\"oic.wk.d\", \"oic.d.light\"],\"host_addr\":\"coap:\\/\\/[fe80::c4a8:5af:7d0e:f40e%25wlan0]:49244\"}]}}";		
-		JSONObject receivedJsonObj = new JSONObject(buffer);
-		logger.debug(" 2 received response json = " + receivedJsonObj.toString());
-		//Import jsonObject
-		try {
-			Schema0Cmd recvSchema0Cmd = new Schema0Cmd();
-			recvSchema0Cmd.importFromJson(receivedJsonObj);
-			
-			JSONObject jsonExportBody = recvSchema0Cmd.exportToJson();
+			jsonExportBody = recvSchema0Cmd.exportToJson();
 			if (jsonExportBody.toString().equals(receivedJsonObj.toString()))
-				logger.debug(" 2 after import = equal ? true ^O^)/");
+				logger.debug(" [test code] ok.. receivedJson == imported data ");
 			else
-				logger.error(" 2 after import = " + jsonExportBody.toString());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				logger.error(" [test code] error.. imported data is not equal = " + jsonExportBody.toString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	static void sendReqDiscoveryResource() {
-		Schema0Cmd sendReqSchema0Cmd = new Schema0Cmd();
+
+		//-- 3.[Send REQ] Discovery Resource
+		//{"type":"req","dir":"d2c","work_code":"dis_res","body":{"dev_id":"6341cb6f-2179-55a3-3732-c3ffbad1be68"}}
+		sendReqSchema0Cmd = new Schema0Cmd();
 		sendReqSchema0Cmd.type = "req";
 		sendReqSchema0Cmd.direction = "d2c";
 		sendReqSchema0Cmd.work_code = "dis_dev";
 		sendReqSchema0Cmd.body = new Schema1Body();
 		sendReqSchema0Cmd.body.device_id = "6341cb6f-2179-55a3-3732-c3ffbad1be68";
+		sendCommand(sendReqSchema0Cmd);
+
+		//-- 4.[Recv Resp] Discovery Resource Result
+		//{"type":"res","dir":"c2d","work_code":"dis_res","body":{"status":200,"resources":[{"res_id":"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0","res_type":["oic.r.switch.binary"],"res_uri":"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0","res_isobserve":true,"res_dev_id":"6341cb6f-2179-55a3-3732-c3ffbad1be68"}]}}
+		receivedJsonObj = recvCommand(2);
+		recvSchema0Cmd = JsonObj2EntityX(receivedJsonObj);
+		try {
+			jsonExportBody = recvSchema0Cmd.exportToJson();
+			if (jsonExportBody.toString().equals(receivedJsonObj.toString()))
+				logger.debug(" [test code] ok.. receivedJson == imported data ");
+			else
+				logger.error(" [test code] error.. imported data is not equal = " + jsonExportBody.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static void sendCommand(Schema0Cmd cmd) {
 		// Export jsonObject
 		try {
-			JSONObject JsonObj4Req = sendReqSchema0Cmd.exportToJson();
+			JSONObject JsonObj4Req = cmd.exportToJson();
 			// check exported data
-			logger.debug(" 1 send request = " + JsonObj4Req.toString());
+			logger.debug(" send cmd = " + JsonObj4Req.toString());
+			
+			// send command to ...
+			// to do ...
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 	}
 	
-	static void recvRespDiscoveryResource() {
-		String buffer = "{\"type\":\"res\",\"dir\":\"c2d\",\"work_code\":\"dis_res\",\"body\":{\"status\":200,\"resources\":[{\"res_id\":\"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0\",\"res_type\":[\"oic.r.switch.binary\"],\"res_uri\":\"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0\",\"res_isobserve\":true,\"res_dev_id\":\"6341cb6f-2179-55a3-3732-c3ffbad1be68\"}]}}";		
-		JSONObject receivedJsonObj = new JSONObject(buffer);
-		logger.debug(" 2 received response json = " + receivedJsonObj.toString());
-		// Import jsonObject
+	static JSONObject recvCommand(int num) {
+		JSONObject receivedJsonObj;
+		String buffer = null;
+		
+		switch (num) {
+		case 1:
+			buffer = "{\"type\":\"res\",\"dir\":\"c2d\",\"work_code\":\"dis_dev\",\"body\":{\"status\":200,\"devices\":[{\"dev_id\":\"c31e8fa3-b524-0e6b-2489-77760c3ca37b\",\"dev_name\":\"THU Light\",\"spec_ver\":\"ocf.1.1.0\",\"dev_type\":[\"oic.wk.d\", \"oic.d.light\"],\"host_addr\":\"coap:\\/\\/[fe80::c4a8:5af:7d0e:f40e%25wlan0]:49244\"}]}}";
+		case 2:
+			buffer = "{\"type\":\"res\",\"dir\":\"c2d\",\"work_code\":\"dis_res\",\"body\":{\"status\":200,\"resources\":[{\"res_id\":\"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0\",\"res_type\":[\"oic.r.switch.binary\"],\"res_uri\":\"/oic/route/6341cb6f-2179-55a3-3732-c3ffbad1be68/power/0\",\"res_isobserve\":true,\"res_dev_id\":\"6341cb6f-2179-55a3-3732-c3ffbad1be68\"}]}}";
+		}
+		receivedJsonObj = new JSONObject(buffer);
+		logger.debug(" received cmd = " + receivedJsonObj.toString());
+		
+		return receivedJsonObj;
+	}
+	
+	static Schema0Cmd JsonObj2EntityX(JSONObject receivedJsonObj) {
+		Schema0Cmd recvSchema0Cmd = new Schema0Cmd();
+		//Import jsonObject
 		try {
-			Schema0Cmd recvSchema0Cmd = new Schema0Cmd();
 			recvSchema0Cmd.importFromJson(receivedJsonObj);
-
-			JSONObject jsonExportBody = recvSchema0Cmd.exportToJson();
-			if (jsonExportBody.toString().equals(receivedJsonObj.toString()))
-				logger.debug(" 2 after import = equal ? true ^O^)/");
-			else
-				logger.error(" 2 after import = " + jsonExportBody.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -115,5 +116,6 @@ public class DevMgr {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return recvSchema0Cmd;
 	}
 }
