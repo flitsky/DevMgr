9eaf83ac702c07fd66e408e89740c44d350ec433
Ryan Kim <flitsky@naver.com>
2017-09-24 15:47:33 +0900

    Add thread into the CmdProcessObserver

--- src/aries/MessageProcess/CmdProcessObserver.java
+++ src/aries/MessageProcess/CmdProcessObserver.java
@@ -4,6 +4,7 @@ import java.util.Observable;
 import java.util.Observer;
 
 import aries.DeviceManager.Message;
+import io.dase.network.DamqRcvProducer;
 
 public abstract class CmdProcessObserver implements Observer {
 	private String ResponseMsgID = "";
@@ -18,6 +19,19 @@ public abstract class CmdProcessObserver implements Observer {
 		observable.addObserver(this);
 		this.ResponseMsgID = msg.getMessageID();
 		CmdProc(msg);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (!ResponseMsgID.equals("")) {
+					try {
+						System.out.println("      Waiting Response... id=" + ResponseMsgID);
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
 	}
 
 	@Override
@@ -27,6 +41,7 @@ public abstract class CmdProcessObserver implements Observer {
 			System.out.println("[5555 Response is arrived] <----- msg : " + responseMessage.getMessage());
 			ResponseProc(responseMessage.getMessage());
 			responseMessage.takeMessage();
+			ResponseMsgID = "";
 			observable.deleteObserver(this);
 			System.out.println("[**** DeleteObserver]");
 		}
