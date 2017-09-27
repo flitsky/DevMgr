512aa611636c0d2a4c7f8fd1758b550e72e75892
Ryan Kim <flitsky@naver.com>
2017-09-27 18:25:19 +0900

    Add ObservableRsrc
    Add ObservableRsrc.java into the TriggerManager package.

---
+++ src/aries/TriggerManager/ObservableRsrc.java
@@ -0,0 +1,26 @@
+package aries.TriggerManager;
+
+import java.util.Observable;
+
+public class ObservableRsrc extends Observable {
+	private String resourceId = "";
+
+	public ObservableRsrc(String rsrcId) {
+		this.resourceId = rsrcId;
+	}
+
+	private void setResourceID(String rsrcId) {
+		this.resourceId = rsrcId;
+	}
+
+	public String getResourceID() {
+		return resourceId;
+	}
+
+	private void onChanged(String rsrcId) {
+		if (this.resourceId == rsrcId) {
+			setChanged();
+			notifyObservers();
+		}
+	}
+}
